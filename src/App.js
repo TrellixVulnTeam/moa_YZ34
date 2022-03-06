adbfe8698d0a36246f31d241fdf7590822e53ba2
zehra3 <zehrataqwi@gmail.com>
2022-03-06 17:05:55 +0300

    feature about us page ui complete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import Events from "./Events";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Events from './Events';
+import AboutUs from './AboutUs';
 
 function App() {
-  return <><Events /></>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path='/events' element={<Events />} />
+          <Route path='/about-us' element={<AboutUs />} />
+          <Route path='/' />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
